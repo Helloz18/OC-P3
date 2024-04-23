53c007f088592e716d4dd562afe283defd08dbb0
Héloïse Finoux-Nikolic <h.finoux-nikolic@ik.me>
2024-04-23 15:53:56 +0200

    add login and register controller

--- src/main/java/com/chatop/api/controller/LoginController.java
+++ src/main/java/com/chatop/api/controller/LoginController.java
@@ -1,12 +1,17 @@
 package com.chatop.api.controller;
 
 import com.chatop.api.security.JwtService;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Tag(name = "Login controller", description = "Endpoint used to log in the application")
+@RequestMapping(produces = MediaType.APPLICATION_JSON_VALUE)
 public class LoginController {
 
     @Autowired
