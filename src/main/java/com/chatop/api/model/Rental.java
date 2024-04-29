eb71aa0246037916402cd431012f25a577036318
Héloïse Finoux-Nikolic <h.finoux-nikolic@ik.me>
2024-04-29 21:06:00 +0200

    update Rental Entity

--- src/main/java/com/chatop/api/model/Rental.java
+++ src/main/java/com/chatop/api/model/Rental.java
@@ -1,6 +1,9 @@
 package com.chatop.api.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
+
+import java.sql.Timestamp;
 
 @Entity
 @Table(name = "rentals")
@@ -11,28 +14,33 @@ public class Rental {
     @Column(name = "id", unique = true, nullable = false)
     private int id;
 
-    @Column(name = "name")
+    @Column(name = "name", nullable = false)
     private String name;
 
-    @Column(name = "surface")
+    @Column(name = "surface", nullable = false)
     private int surface;
 
-    @Column(name = "price")
+    @Column(name = "price", nullable = false)
     private long price;
 
     @Column(name = "picture")
     private String picture;
 
-    @Column(name= "description")
+    @Column(name= "description", nullable = false)
     private String description;
 
     @ManyToOne @JoinColumn(name="owner_id", nullable=false)
     private User user;
 
-    @Column(name = "created_at")
-    private String createdAt;
-    @Column(name = "updated_at")
-    private String updatedAt;
+    @Column(name="created_at", columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
+    @JsonProperty("created_at")
+    private Timestamp createdAt;
+    @Column(name="updated_at", columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
+    @JsonProperty("updated_at")
+    private Timestamp updatedAt;
+
+    public Rental(){
+    }
 
     public Rental(String name, int surface, long price, String picture, String description){
         this.name = name;
@@ -105,19 +113,29 @@ public class Rental {
         this.user = user;
     }
 
-    public String getCreatedAt() {
+
+    //Timestamp in db will be stored as yyyy-MM-dd HH:mm:ss
+    public Timestamp getCreatedAt() {
         return createdAt;
     }
-
     public void setCreatedAt(String createdAt) {
-        this.createdAt = createdAt;
+        String createdAtConvert = this.convertTimestampFromOpenApi(createdAt);
+        this.createdAt = Timestamp.valueOf(createdAtConvert);
     }
 
-    public String getUpdatedAt() {
+    public Timestamp getUpdatedAt() {
         return updatedAt;
     }
 
     public void setUpdatedAt(String updatedAt) {
-        this.updatedAt = updatedAt;
+        String updatedAtConvert = this.convertTimestampFromOpenApi(updatedAt);
+        this.updatedAt = Timestamp.valueOf(updatedAtConvert);
+
+    }
+
+    private String convertTimestampFromOpenApi(String openApiTimestamp) {
+        String convert = openApiTimestamp.replace('T', ' ');
+        convert = convert.replace('Z', ' ');
+        return convert;
     }
 }
