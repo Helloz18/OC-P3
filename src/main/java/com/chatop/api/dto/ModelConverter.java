fb498689255af64292748e149874bd6c8627d5e4
Héloïse Finoux-Nikolic <h.finoux-nikolic@ik.me>
2024-04-29 21:10:36 +0200

    correct toUserDTO and add toRentalDTO in ModelConverter

--- src/main/java/com/chatop/api/dto/ModelConverter.java
+++ src/main/java/com/chatop/api/dto/ModelConverter.java
@@ -1,12 +1,22 @@
 package com.chatop.api.dto;
 
+import com.chatop.api.model.Rental;
 import com.chatop.api.model.User;
+import com.chatop.api.repository.UserRepository;
+import com.chatop.api.security.JwtService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.time.Instant;
 
 @Component
 public class ModelConverter {
+
+    @Autowired
+    private static JwtService jwtService;
+
+    @Autowired
+    private static UserRepository userRepository;
 
     public static User toUserCreate(final UserDTO userDTO) {
         User user = new User(userDTO.getEmail(), userDTO.getName(), userDTO.getPassword());
@@ -18,5 +28,25 @@ public class ModelConverter {
         User user = new User(userDTO.getEmail(), userDTO.getName(), userDTO.getPassword());
         user.setUpdatedAt(Instant.now().toString());
         return user;
+    }
+
+    public static Rental toRentalCreate(final RentalDTO rentalDTO) {
+        Rental rental =
+                new Rental(rentalDTO.getName(),
+                           rentalDTO.getSurface(),
+                           rentalDTO.getPrice(),
+                           rentalDTO.getPicture(),
+                           rentalDTO.getDescription());
+        rental.setCreatedAt(Instant.now().toString());
+        return rental;
+    }
+
+    public static Rental toRentalUpdate(final RentalDTO rentalDTO) {
+        Rental rental =
+                new Rental(rentalDTO.getName(),
+                           rentalDTO.getSurface(),
+                           rentalDTO.getPrice(),
+                           rentalDTO.getDescription());
+        return rental;
     }
 }
