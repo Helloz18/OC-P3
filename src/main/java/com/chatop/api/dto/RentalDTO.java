00576a22c74dd6284341aca85160fd90cfde7a8f
Héloïse Finoux-Nikolic <h.finoux-nikolic@ik.me>
2024-04-29 21:06:48 +0200

    update RentalDTO

--- src/main/java/com/chatop/api/dto/RentalDTO.java
+++ src/main/java/com/chatop/api/dto/RentalDTO.java
@@ -1,9 +1,12 @@
 package com.chatop.api.dto;
 
-import jakarta.persistence.Column;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.sql.Timestamp;
 
 public class RentalDTO {
 
+    private int id;
     private String name;
 
     private int surface;
@@ -13,6 +16,17 @@ public class RentalDTO {
     private String picture;
 
     private String description;
+
+    @JsonProperty("owner_id")
+    private int ownerId;
+
+    @JsonProperty("created_at")
+    private Timestamp createdAt;
+
+    @JsonProperty("updated_at")
+    private Timestamp updatedAt;
+
+    public RentalDTO() {}
 
     public RentalDTO(String name, int surface, long price, String description) {
         this.name = name;
@@ -27,6 +41,24 @@ public class RentalDTO {
         this.price = price;
         this.picture = picture;
         this.description = description;
+    }
+
+    public RentalDTO(String name, int surface, long price, String picture, String description, int ownerId) {
+        this.name = name;
+        this.surface = surface;
+        this.price = price;
+        this.picture = picture;
+        this.description = description;
+        this.ownerId = ownerId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id =
+                id;
     }
 
     public String getName() {
@@ -67,5 +99,34 @@ public class RentalDTO {
 
     public void setDescription(String description) {
         this.description = description;
+    }
+
+    public void setOwnerId(int ownerId) {
+        this.ownerId = ownerId;
+    }
+
+    //Timestamp in db will be stored as yyyy-MM-dd HH:mm:ss
+    public Timestamp getCreatedAt() {
+        return createdAt;
+    }
+    public void setCreatedAt(String createdAt) {
+        String createdAtConvert = this.convertTimestampFromOpenApi(createdAt);
+        this.createdAt = Timestamp.valueOf(createdAtConvert);
+    }
+
+    public Timestamp getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(String updatedAt) {
+        String updatedAtConvert = this.convertTimestampFromOpenApi(updatedAt);
+        this.updatedAt = Timestamp.valueOf(updatedAtConvert);
+
+    }
+
+    private String convertTimestampFromOpenApi(String openApiTimestamp) {
+        String convert = openApiTimestamp.replace('T', ' ');
+        convert = convert.replace('Z', ' ');
+        return convert;
     }
 }
