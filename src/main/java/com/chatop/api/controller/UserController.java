88d2845935f49b3f789a950b917782d0cce72b8e
Héloïse Finoux-Nikolic <h.finoux-nikolic@ik.me>
2024-05-01 09:38:02 +0200

    add description for endpoint /api/user/{id}

--- src/main/java/com/chatop/api/controller/UserController.java
+++ src/main/java/com/chatop/api/controller/UserController.java
@@ -1,7 +1,14 @@
 package com.chatop.api.controller;
 
 import com.chatop.api.dto.UserDTO;
+import com.chatop.api.model.ResponseMessage;
 import com.chatop.api.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,7 +25,18 @@ public class UserController {
     private UserService userService;
 
     @GetMapping("/{id}")
-    public User getUserById(@PathVariable int id) throws Exception {
+    @Operation(summary = "Get the user by its id.",
+            description = "Returns a user." )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "A user",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = User.class))),
+            @ApiResponse(responseCode = "401",
+                    content = @Content(mediaType = "application/json"))
+    })
+    public User getUserById(
+            @Parameter(description = "id of the user", example="5")
+            @PathVariable int id) throws Exception {
         return userService.getUserById(id);
     }
 }
